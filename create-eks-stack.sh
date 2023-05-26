e8a622adb52d3ba3af40a08741433a0aee5e02e3
Jeff <36047849+thecodebeneath@users.noreply.github.com>
2023-05-26 11:41:33 -0500

    change region to us-east-1

--- create-eks-stack.sh
+++ create-eks-stack.sh
@@ -1,5 +1,5 @@
 aws cloudformation create-stack \
-  --region eu-central-1 \
+  --region us-east-1 \
   --stack-name my-eks-cluster \
   --capabilities CAPABILITY_NAMED_IAM \
   --template-body file://eks-stack.yaml 
